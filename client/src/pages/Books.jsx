69d449751461db82e228acd6c79aa226c654d0d8
Brewer <12806kight@gmail.com>
2023-11-17 01:46:47 -0600

    This shows the list!

--- client/src/pages/Books.jsx
+++ client/src/pages/Books.jsx
@@ -6,8 +6,8 @@ function Books() {
     useEffect(()=>{
         const fetchBooks = async()=>{
             try{
-                const res = await axios.get("https:localhost:8800/books");
-                console.log(res);
+                const res = await axios.get("http://localhost:8800/books");
+                setBooks(res.data)
             }catch(err){
                 console.log(err);
             }
@@ -15,7 +15,15 @@ function Books() {
         fetchBooks();
     }, [])
   return (
-    <div>Books</div>
+    <div>
+        <h1>Hello World!</h1>
+        {books.map(book=>(
+            <div key={book.PersonID}>
+            <p>{book.FirstName}</p>
+            <p>{book.LastName}</p>
+            </div>
+        ))}
+    </div>
   )
 }
 
