17a96cd2396dc4338581d525b13d4f962fb42642
Brewer <12806kight@gmail.com>
2023-11-16 20:40:59 -0600

    Basic frontend code

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,26 @@
 import express from "express";
+import mysql from "mysql2";
 
 const app = express();
+
+const db = mysql.createConnection({
+    host : "localhost",
+    user : "root",
+    password: process.env.Password,
+    database : "test"
+})
+
+app.get("/", (req, res)=> {
+    res.json("Hello friend!");
+})
+
+app.get("/books", (req, res)=> {
+    const q = "SELECT * FROM books";
+    db.query(q, (err, data)=>{
+        if(err) return res.json(err);
+        return res.json(data);
+    })
+})
 
 app.listen(8800, () =>{
     console.log("Connected to the backend");
