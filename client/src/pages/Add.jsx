01258a7793b6f0cb3df33e359d138a920b6e2b76
Brewer <12806kight@gmail.com>
2023-11-17 17:06:04 -0600

    Added a Handle Click

--- client/src/pages/Add.jsx
+++ client/src/pages/Add.jsx
@@ -3,9 +3,10 @@ import React, {useState} from 'react'
 function Add() {
   const[input, setInput] = useState({
     FirstName : "",
-    LastName : ""
+    LastName : "" 
   })
 
+  
   function handleChange(event){
     const {name , value} = event.target;
         setInput(previousNotes =>{
@@ -15,14 +16,36 @@ function Add() {
             }
         })
   }
+
+  const hc = async(event)=>{
+    event.preventDefault();
+  
+    setInput({
+      FirstName: "",
+      LastName: ""
+    })
+
+    try{
+      const res = await axios.post("http://localhost:8800/books", input);
+      setBooks(res.data)
+  }catch(err){
+      console.log(err);
+  }
+}
+
+    
+  
+
   return (
     <div>
       <form>
         <input type="text" placeholder="FirstName" onChange={handleChange} name="FirstName" value={input.FirstName}></input>
         <input type="text" placeholder="LastName" onChange={handleChange} name="LastName"value={input.LastName}></input>
+        <button onClick={hc}>ADDED</button>
       </form>
+      
     </div>
   )
-}
 
+  }
 export default Add
