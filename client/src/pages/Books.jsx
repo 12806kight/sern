842ae7b14e24626dc1f1c29ec35c37f2b51a837f
Brewer <12806kight@gmail.com>
2023-11-17 01:03:26 -0600

    Updated frontend to allow axios

--- client/src/pages/Books.jsx
+++ client/src/pages/Books.jsx
@@ -1,6 +1,19 @@
-import React from 'react'
+import axios from 'axios';
+import React, {useEffect, useState} from 'react'
 
 function Books() {
+    const [books, setBooks] = useState([]);
+    useEffect(()=>{
+        const fetchBooks = async()=>{
+            try{
+                const res = await axios.get("https:localhost:8800/books");
+                console.log(res);
+            }catch(err){
+                console.log(err);
+            }
+        }
+        fetchBooks();
+    }, [])
   return (
     <div>Books</div>
   )
