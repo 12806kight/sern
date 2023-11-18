e2602db4ba27dea63b940f1387516e167194d5c3
Brewer <12806kight@gmail.com>
2023-11-18 01:23:05 -0600

    Fixed the issue with mySql Workbench.  Took longer than anticipated.

--- backend/index.js
+++ backend/index.js
@@ -14,31 +14,36 @@ const db = mysql.createConnection({
     database : "test"
 })
 
-app.get("/", (req, res)=> {
-    res.json("Hello friend!");
-})
-
-app.get("/books", (req, res)=> {
+app.get("/", (req, res) => {
+    res.json("hello");
+  });
+  
+  app.get("/books", (req, res) => {
     const q = "SELECT * FROM books";
-    db.query(q, (err, data)=>{
-        if(err) return res.json(err);
-        return res.json(data);
-    })
-})
-
-app.post("/books", (req, res)=>{
-    const q = "INSERT INTO books (`title`, `desc`, `cover`) VALUES (?)";
+    db.query(q, (err, data) => {
+      if (err) {
+        console.log(err);
+        return res.json(err);
+      }
+      return res.json(data);
+    });
+  });
+  
+  app.post("/books", (req, res) => {
+    const q = "INSERT INTO books(`title`, `desc`, `cover`) VALUES (?)";
+  
     const values = [
-        req.body.title,
-        req.body.desc,
-        req.body.cover,
-    ]
-
-    db.query(q, [values], (err, data)=>{
-        if(err) return res.json(err);
-        return res.json("Book was created successfully!");
-    })
-})
+      req.body.title,
+      req.body.desc,
+      req.body.cover,
+    ];
+  
+    db.query(q, [values], (err, data) => {
+      if (err) return res.send(err);
+      return res.json(data);
+    });
+  });
+  
 
 app.listen(8800, () =>{
     console.log("Connected to the backend");
